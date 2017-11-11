99ef5cbf39fca5f739da8662b4aba61a3c12b851
Longfei Li <codyli520@gmail.com>
2017-11-11 13:17:27 -0800

    svg added in template

---
+++ js/bar-chart.js
@@ -0,0 +1,5 @@
+var svg = d3.select("svg#svg_1"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
